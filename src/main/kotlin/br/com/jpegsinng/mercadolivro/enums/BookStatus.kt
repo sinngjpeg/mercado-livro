aaaf67c6c25ce0ba2bac1ac0f37c9364513298e8
Ingrid Silva <ingridsilvatec@outlook.com>
2025-05-17 04:10:15 -0300

    adiciona o model de book

---
+++ src/main/kotlin/br/com/jpegsinng/mercadolivro/enums/BookStatus.kt
@@ -0,0 +1,8 @@
+package br.com.jpegsinng.mercadolivro.enums
+
+enum class BookStatus {
+    ATIVO,
+    VENDIDO,
+    CALCELADO,
+    DELETADO
+}
---
+++ src/main/kotlin/br/com/jpegsinng/mercadolivro/model/BookModel.kt
@@ -0,0 +1,31 @@
+package br.com.jpegsinng.mercadolivro.model
+
+import br.com.jpegsinng.mercadolivro.enums.BookStatus
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.math.BigDecimal
+
+@Entity(name = "book")
+data class BookModel(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+    @Column
+    var name: String,
+    @Column
+    var prince: BigDecimal,
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null,
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    var customer: CustomerModel
+)
