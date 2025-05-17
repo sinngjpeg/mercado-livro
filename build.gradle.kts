af8b32f65b36aeddc640e2ddcb21e63ed83b3ca6
Ingrid Silva <ingridsilvatec@outlook.com>
2025-05-17 03:58:11 -0300

    implementa o flyway no projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.mysql:mysql-connector-j")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-mysql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
