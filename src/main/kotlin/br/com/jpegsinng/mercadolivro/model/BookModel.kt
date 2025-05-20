6b53404a2f1fdf72538fe257d438b965545339b1
Ingrid Silva <ingridsilvatec@outlook.com>
2025-05-19 23:14:08 -0300

    bloquando alteracao de status de livros deletados/cancelados

--- src/main/kotlin/br/com/jpegsinng/mercadolivro/model/BookModel.kt
+++ src/main/kotlin/br/com/jpegsinng/mercadolivro/model/BookModel.kt
@@ -1,15 +1,7 @@
 package br.com.jpegsinng.mercadolivro.model
 
 import br.com.jpegsinng.mercadolivro.enums.BookStatus
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
+import jakarta.persistence.*
 import java.math.BigDecimal
 
 @Entity(name = "book")
@@ -18,14 +10,35 @@ data class BookModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int? = null,
+
     @Column
     var name: String,
+
     @Column
     var price: BigDecimal,
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
+
     @ManyToOne
     @JoinColumn(name = "customer_id")
-    var customer: CustomerModel
-)
+    var customer: CustomerModel? = null
+) {
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == BookStatus.CANCELADO || field == BookStatus.DELETADO)
+                throw Exception("Não é possivel alterar um livro com status ${field}")
+
+            field = value
+        }
+
+    constructor(
+        id: Int? = null,
+        name: String,
+        price: BigDecimal,
+        customer: CustomerModel? = null,
+        status: BookStatus?
+    ) : this(id, name, price, customer) {
+        this.status = status
+    }
+}
