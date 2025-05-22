593a47d9cc745195242ce9b6c65c2d121449a661
Ingrid Silva <ingridsilvatec@outlook.com>
2025-05-22 19:11:14 -0300

    WIP: iniciando confgs de segurança

--- src/main/kotlin/br/com/jpegsinng/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/br/com/jpegsinng/mercadolivro/config/SecurityConfig.kt
@@ -2,11 +2,34 @@ package br.com.jpegsinng.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
-
+@EnableWebSecurity
 class SecurityConfig {
+
+    private val PUBLIC_MATCHERS = arrayOf<String>()
+    private val PUBLIC_POST_MATCHERS = arrayOf("/customer")
+
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors {}
+            .csrf { it.disable() }
+            .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers(*PUBLIC_MATCHERS).permitAll()
+                auth.requestMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+                    .anyRequest().authenticated()
+            }.build()
+    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
